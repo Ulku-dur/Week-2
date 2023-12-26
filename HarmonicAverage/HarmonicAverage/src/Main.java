b2033d12176a2865e8b3569cd51a9fc10be56f99
ÜlküDurmazKöker <93094752+Ulku-dur@users.noreply.github.com>
2023-12-26 21:17:51 +0300

    Add files via upload

---
+++ HarmonicAverage/HarmonicAverage/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        double[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0; double result = 0;
+        for (double i : numbers) {
+                result += 1/i;
+            }
+            System.out.println("Dizinin harmonik ortalaması: " +result);
+
+        }
+    }
